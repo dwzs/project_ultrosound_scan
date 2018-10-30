d574236de8c05e3058e21f87942f18782ffd680f
wsy <1214398116@qq.com>
2018-10-30 10:13:06 +0800

    改好了lists_to_poses

--- project_ur5/lib/tools.py
+++ project_ur5/lib/tools.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy, sys
 from geometry_msgs.msg import Pose
+from copy import deepcopy
 
 class Tools:
     def __init__(self):
@@ -8,27 +9,18 @@ class Tools:
 
 
     @staticmethod
-    def lists_to_waypoints(lists):
+    def lists_to_poses(lists):
         pose = Pose()
         waypoints = []
 
         for lst in lists:
-            # pose.position.x = lst[0]
             pose.position.x = lst[0]
-            # pose.position.y = lst[1]            
-            # pose.position.z = lst[2]            
-            # pose.orientation.x = lst[3]
-            # pose.orientation.y = lst[4]
-            # pose.orientation.z = lst[5]
-
-
-            # print('lst:',lst)
-            # print ('pose:',pose)            
-            waypoints.append(pose)
-            # print ('waypoints:',waypoints)
-
-
-
+            pose.position.y = lst[1]            
+            pose.position.z = lst[2]            
+            pose.orientation.x = lst[3]
+            pose.orientation.y = lst[4]
+            pose.orientation.z = lst[5]       
+            waypoints.append(deepcopy(pose))
         return waypoints                               
 
 if __name__ == "__main__":
@@ -40,5 +32,5 @@ if __name__ == "__main__":
     demo_lists = [demo_list1, demo_list2, demo_list3]
 
     waypoints = Tools.lists_to_waypoints(demo_lists)
-    rospy.loginfo('1111')
+    
     print (waypoints)
